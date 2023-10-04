8c2d83026e910a5446e35bc1969e979a42e50048
Trobinson13 <trerrobinson@gmail.com>
2023-10-04 19:58:15 -0400

    more

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,4 +1,4 @@
-SELECT id as Movie_ID, name AS Movie_Tilte, imdb_rating AS Rating
+SELECT id as Movie_ID, name as Movie_Title, imdb_rating as Rating
 FROM movies
 WHERE genre = 'horror' AND year <= 1985
 ORDER BY imdb_rating DESC
